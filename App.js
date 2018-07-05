808a5b86364209e67e58ff5126b237a17faaae28
kevin <kevin_rojas30@hotmail.com>
2018-07-04 19:00:19 -0500

    Initial commit

---
+++ App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { 
+  StyleSheet, 
+  Text, 
+  View 
+} from 'react-native';
+
+import EstadoCuenta  from './src/components/EstadoCuenta';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <EstadoCuenta/>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ src/components/EstadoCuenta.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { 
+    StyleSheet, 
+    Text, 
+    View 
+} from 'react-native';
+export default class EstadoCuenta extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.header}>
+            <Text style={styles.saldoText}>$0.00 </Text>
+        </View>
+        <View style={styles.content}>
+        </View>
+
+      </View>
+    );
+  }
+}
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    saldoText:{
+        fontSize:16,
+        color:'white',
+        fontWeight: "bold",
+        padding: 10,
+        justifyContent: 'center',
+       
+        alignItems:'center',
+    },
+    header:{
+        backgroundColor:'#455A64',
+        flex:1,
+        //borderWidth:1,
+        marginTop:10,
+        
+    },
+    content:{
+        flex:5,
+        borderWidth:1,
+    }
+});
